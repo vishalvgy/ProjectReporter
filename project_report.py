4f028d3f04c6548ff123ba105c8fb763c58f65b7
Vishal <vishal@karthavya.com>
2016-04-21 13:06:56 +0530

    Added project path checking.

--- project_report.py
+++ project_report.py
@@ -8,15 +8,18 @@
 #Karthavya Technologies Pvt. Ltd.
 
 import sys
+import os
 
 from git_operations import *
 
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
        sys.exit("USAGE: ./poject_report.py <Project Path>")
 
     project = sys.argv[1]
+    if not os.path.exists(project):
+        sys.exit("Project <%s> does not exists..!!"%project)
 
     print "Report:",get_project_contribution_report(project)
